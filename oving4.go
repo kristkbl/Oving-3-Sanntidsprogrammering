a535e4ab3783d0c8df2e6315710a98f63271271d
student <student@Sanntid.(none)>
2014-02-05 12:05:55 +0100

    jobbet videre 5. feb

--- oving4.go
+++ oving4.go
@@ -3,30 +3,31 @@ package main
 import (
         "net"
         "fmt"
-    //"io"
-    //"os"
-        //"bufio"
+		"time"
 )
 
 
 func main(){
 
-    
     // TCP
     // CLIENT
-    go tcp_client()
+    //go tcp_client()
 
     
     // SERVER
-    go tcp_server()
+    //go tcp_server()
     
+	// UDP
+	
 
-    // UDP
+	//go udp_reciver()
 
-    udp_reciver()
+	go udp_sender()
+
+
+	neverQuit := make(chan string)
+	<- neverQuit
     
-
-
 }
 
 func tcp_receiver(c net.Conn) {
@@ -38,17 +39,12 @@ func tcp_receiver(c net.Conn) {
     }
 }
 
-
 func tcp_client() {
     serverAddr, err := net.ResolveTCPAddr("tcp", "129.241.187.161:34933")
-    if err != nil {
-         fmt.Println(err)
-    }
+    PrintError(err)
 
     con, err := net.DialTCP("tcp", nil, serverAddr);
-    if err != nil {
-         fmt.Println(err)
-    }
+    PrintError(err)
     
     
     go tcp_receiver(con)
@@ -60,19 +56,15 @@ func tcp_client() {
     con.Write([]byte("Connect to: 129.241.187.155:34933\x00"))
 }
 
-
-
 func tcp_server() {
     listener, err := net.Listen("tcp", ":34933")
-    if err != nil {
-        fmt.Println(err)
-    }
+    PrintError(err)
+
     defer listener.Close()
     for {
         newCon, err := listener.Accept()
-        if err != nil {
-            fmt.Println(err)
-        }
+        PrintError(err)
+
         go tcp_receiver(newCon)
         newCon.Write([]byte("nc:hello tcp world\x00"))
         newCon.Write([]byte("nc:so fancy\x00"))
@@ -81,50 +73,70 @@ func tcp_server() {
 }
 
 
-
 func udp_reciver() {
     newBuf := make([]byte, 1024)
     addr, err := net.ResolveUDPAddr("udp", ":20020")
-    if err != nil{
-	fmt.Println(err)
-    }
+    PrintError(err)
+
     sock, err := net.ListenUDP("udp", addr)
-    if err != nil{
-        fmt.Println(err)
-    }
-    
-    // syntaksfeil her:
-    upd_sender(newBuf []byte, sock *UDPConn)
-}
+	PrintError(err)
 
-func udp_sender(newBuf []byte, sock *UDPConn) {
-    serverAddr_udp, err := net.ResolveUDPAddr("udp", "129.241.187.255:20020")
-    if err != nil{
-        fmt.Println(err)
-    }
-    con_udp, err := net.DialUDP("udp", nil, serverAddr_udp)
-    if err != nil{
-        fmt.Println(err)
-    }
-
-
-    _, err2 := con_udp.Write([]byte("hello udp world"))
-    if err2 != nil{
-        fmt.Println(err2)
-    }
-    fmt.Println("Du har kommet helt hit")
-
-    
-    for {
+	for {
         _, _, err = sock.ReadFromUDP(newBuf)
-        if err != nil{
-            fmt.Println(err)
-        }
+        PrintError(err)
+
         fmt.Println(string(newBuf))
     }
-
-
 }
+
+func udp_sender() {
+    serverAddr_udp, err := net.ResolveUDPAddr("udp", "129.241.187.255:20020")
+	PrintError(err)
+
+    con_udp, err := net.DialUDP("udp", nil, serverAddr_udp)
+    PrintError(err)
+
+	for {
+		time.Sleep(1000 * time.Millisecond)
+		_, err2 := con_udp.Write([]byte("Fremdeles pa?"))
+		PrintError(err2)
+	}
+}
+
+
+func PrintError(err error) {
+	if err != nil{
+        fmt.Println(err)
+	}
+}
+
+// udp_sender(newBuf []byte, sock *net.UDPConn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
